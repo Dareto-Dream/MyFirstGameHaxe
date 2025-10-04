fb17e8c633b8b40d328d5c904272bdf9baf9637c
Dareto-Dream <dare.to.0ream@gmail.com>
2025-10-03 21:05:01 -1000

    animation

--- source/Player.hx
+++ source/Player.hx
@@ -14,6 +14,16 @@ class Player extends FlxSprite
         loadGraphic(AssetPaths.player__png, true, 16, 16);
         setFacingFlip(LEFT, false, false);
         setFacingFlip(RIGHT, true, false);
+        setSize(8, 8);
+        offset.set(4, 8);
+
+        animation.add("d_idle", [0]);
+        animation.add("lr_idle", [3]);
+        animation.add("u_idle", [6]);
+        animation.add("d_walk", [0, 1, 0, 2], 6);
+        animation.add("lr_walk", [3, 4, 3, 5], 6);
+        animation.add("u_walk", [6, 7, 6, 8], 6);
+
         drag.x = drag.y = 800;
 	}
 
@@ -44,6 +54,7 @@ class Player extends FlxSprite
                     newAngle -= 45;
                 else if (right)
                     newAngle += 45;
+                facing = UP;
             }
             else if (down)
             {
@@ -52,13 +63,39 @@ class Player extends FlxSprite
                     newAngle += 45;
                 else if (right)
                     newAngle -= 45;
+                facing = DOWN;
             }
             else if (left)
+            {
                 newAngle = 180;
+                facing = LEFT;
+            }
             else if (right)
+            {
                 newAngle = 0;
+                facing = RIGHT;
+            }
 
+            // determine our velocity based on angle and speed
             velocity.setPolarDegrees(SPEED, newAngle);
+        }
+
+        var action = "idle";
+        // check if the player is moving, and not walking into walls
+        if ((velocity.x != 0 || velocity.y != 0) && touching == NONE)
+        {
+            action = "walk";
+        }
+
+        switch (facing)
+        {
+            case LEFT, RIGHT:
+                animation.play("lr_" + action);
+            case UP:
+                animation.play("u_" + action);
+            case DOWN:
+                animation.play("d_" + action);
+            case _:
         }
     }
 
